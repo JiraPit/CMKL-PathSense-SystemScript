3f06a875df32dda94253a11c8917998a32a385ef
JiraPit <pitakwongjira@gmail.com>
2023-11-19 16:24:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,12 +27,12 @@ sudo cp -r SystemScript /
 echo "PathSense system script added to root directory"
 
 # Check if main.py is already in the boot file
-if grep -Fxq "cd /SystemScript && python main.py &" /etc/rc.local
+if grep -Fxq "cd /SystemScript && sudo python main.py &" /etc/rc.local
 then
     echo "PathSense system script is already set to run at boot"
 else
     # If not, add the command to the boot file
-    sed -i '$i cd /SystemScript && python main.py &' /etc/rc.local
+    sed -i '$i cd /SystemScript && sudo python main.py &' /etc/rc.local
     echo "PathSense system script is now set to run at boot"
 fi
 
